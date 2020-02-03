5192e13680404edf44e1a4779c8dd80642e177aa
Artfenix <hugo11.carlsson@gmail.com>
2020-02-03 15:21:18 +0100

    Tower and GameLogic Scripts Made
    Made Scripts

---
+++ Unity/Project Tree Theft/Assets/Scripts/GameLogic.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameLogic : MonoBehaviour
+{
+
+
+
+
+    static private int collectedLogs;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+    public static void GameOverLose()
+    {
+        SceneManager.LoadScene("GameOver");
+    }
+
+    public static void GameOverWin()
+    {
+        SceneManager.LoadScene("Victory");
+    }
+    public static void AddLog()
+    {
+        collectedLogs++;
+    }
+    public static void RemoveLog()
+    {
+        collectedLogs--;
+    }
+}

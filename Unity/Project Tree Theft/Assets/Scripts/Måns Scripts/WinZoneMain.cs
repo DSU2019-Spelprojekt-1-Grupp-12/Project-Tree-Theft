3b8527c3b3c45f55511a10a43cac21727b9c64f5
MKRosenfors <mans.rosenfors@live.se>
2020-02-04 11:03:40 +0100

    WinZone add

---
+++ Unity/Project Tree Theft/Assets/Scripts/Måns Scripts/WinZoneMain.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinZoneMain : MonoBehaviour
+{
+    #region Components
+
+    #endregion
+
+    #region Variables
+
+    #endregion
+
+    #region Core Functions
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Log"))
+        {
+            GameLogic.AddLog();
+            Debug.Log("LogAdded");
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Log"))
+        {
+            GameLogic.RemoveLog();
+            Debug.Log("LogRemoved");
+        }
+    }
+    #endregion
+
+    #region Functions
+
+    #endregion
+
+}

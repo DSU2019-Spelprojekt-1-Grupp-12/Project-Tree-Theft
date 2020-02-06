d35aae3a5621bcac3d4d43e438fddacfedee0806
MKRosenfors <mans.rosenfors@live.se>
2020-02-06 13:22:16 +0100

    Park Ranger Init

---
+++ Unity/Project Tree Theft/Assets/Scripts/Måns Scripts/RangerMain.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RangerMain : MonoBehaviour
+{
+    #region Components
+
+    #endregion
+
+    #region Variables
+
+    GameObject[] players;
+    GameObject playerOne;
+    GameObject playerTwo;
+    #endregion
+
+    #region Core Functions
+    void Start()
+    {
+        initializeRanger();
+    }
+    void Update()
+    {
+
+    }
+    #endregion
+
+    #region Functions
+    void initializeRanger()
+    {
+        players = GameObject.FindGameObjectsWithTag("Player");
+        playerOne = players[0];
+        playerTwo = players[1];
+    }
+
+    #endregion
+
+}

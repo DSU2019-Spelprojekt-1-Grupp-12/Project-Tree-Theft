57047f1f5a214372d40308ac68b43804e2f89daf
Edison Kardirsson <ed.kardirsson@hotmail.com>
2020-02-16 19:08:43 +0100

    Menu system implemented

--- Unity/Project Tree Theft/Assets/Scripts/Menu Scripts/MenuManager.cs
+++ Unity/Project Tree Theft/Assets/Scripts/Menu Scripts/MenuManager.cs
@@ -27,28 +27,27 @@ public class MenuManager : MonoBehaviour
     
     private string s_LoadLevel_n = "Level";
 
-    private void Update(){
-        
-    }
-
-    public void LoadLevel(int levelNumber){
-        s_LoadLevel_n += levelNumber;
-        SceneManager.LoadScene(s_LoadLevel_n);
-    }
-
+    private void Update(){}
 
     [HideInInspector] public void Load(string name){
         SceneManager.LoadScene(name);
         Debug.Log("Loaded " + name);
     }
 
+    public void SplashScreen() { Load(s_Splash); }
     public void NewGame() { Load(s_New); }
     public void LoadGame() { Load(s_Load); }
     public void Options() { Load(s_Opt); }
     public void Credits() { Load(s_Credits); }
+    public void MainMenu() { Load(s_Main); }    
     public void LoadGameOverWin() { Load(s_GOWin); }
     public void LoadGameOverLose() { Load(s_GOlose); }
 
+    public void LoadLevel(int levelNumber){
+        s_LoadLevel_n += levelNumber;
+        SceneManager.LoadScene(s_LoadLevel_n);
+    }
+    
     public void QuitGame(){
         Application.Quit();
         Debug.Log("Game is shutdown!");

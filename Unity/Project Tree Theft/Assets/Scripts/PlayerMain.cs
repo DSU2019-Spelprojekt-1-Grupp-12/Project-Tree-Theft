a3fe6f9ed79401d10111da9cd54ef1b1b7028910
MKRosenfors <mans.rosenfors@live.se>
2020-03-02 11:12:40 +0100

    'fix'fix

--- Unity/Project Tree Theft/Assets/Scripts/PlayerMain.cs
+++ Unity/Project Tree Theft/Assets/Scripts/PlayerMain.cs
@@ -213,11 +213,11 @@ public class PlayerMain : MonoBehaviour
             movementVector.y = 0;
             if (GetDirectionSprite() == 2 && rigidBody.velocity.magnitude > 0)
             {
-                SendMessage("toggleWalking", true);
+                SendMessage("toggleWalkingDown", true);
             }
             else
             {
-                SendMessage("toggleWalking", false);
+                SendMessage("toggleWalkingDown", false);
             }
         }
         else if (attached == true)
